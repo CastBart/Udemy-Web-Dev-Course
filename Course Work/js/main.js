aaabfdc39719a0a3a7070e0e25d625b2b2f78bb6
CastBart <bartosz.j.zych@gmail.com>
2022-02-25 13:01:34 +0100

    changing value of an element using javascript

--- Course Work/js/main.js
+++ Course Work/js/main.js
@@ -3,6 +3,9 @@ let myBtn = document.getElementById("go-button");
 function buttonClicked(){
     console.log("Button Clicked");
     myBtn.removeEventListener("click", buttonClicked);
+    document.getElementById("text").innerHTML = "I changed the value stored in this element";
+    console.log(document.getElementById("text").innerHTML);
+
 }
 
 
