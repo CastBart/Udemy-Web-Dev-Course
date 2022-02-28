047161270ecd8f6badc58455c6b5cc8b456c9db2
CastBart <bartosz.j.zych@gmail.com>
2022-02-28 12:09:29 +0100

    simple difference between jquery and js

--- Course Work/js/main.js
+++ Course Work/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
     alert("Loaded");
-
-
+    document.getElementById("text").innerHTML = "this is my text";
+    $("#text").html("This is now my text");
+    $(".my-input").val("new input value");
 });
