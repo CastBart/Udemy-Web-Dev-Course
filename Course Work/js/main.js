47d3196e5de38d633e687f7c189c7d9396289a1b
CastBart <bartosz.j.zych@gmail.com>
2022-02-28 11:51:04 +0100

    setting up jquery

--- Course Work/js/main.js
+++ Course Work/js/main.js
@@ -1,20 +1,6 @@
-function missionNumber(array){
+$(document).ready(function(){
 
-    
-    let myArr = [...array];
-    myArr.sort((a,b) => a-b);
-    console.log(myArr);
-    for(let i = myArr[0]; i < myArr.length; i++){
-        if(i !== myArr[i]){
-            return i;
-        }
-    }
-    return "No missing numbers";
-}
+    alert("Loaded");
 
 
-
-let numbers = [3,8,7,9,6,0,1,12,5,14,4,2,19,16,18,11,10,15,13,17,20];
-
-
-console.log(missionNumber(numbers));
+});
