d5397650c0330cf6625f67b5a357033f5b2cfef7
CastBart <bartosz.j.zych@gmail.com>
2022-03-01 10:19:36 +0100

    "which" property of an event

--- Course Work/js/main.js
+++ Course Work/js/main.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
 
+    let el = document.getElementById("text");
+
+    $(document).on("mousedown", function (event) {
+        event.preventDefault();
+        switch(event.which){
+            case 1:
+                console.log("Clicked left mouse button");
+                break;
+            case 2:
+                console.log("Clicked the scroll wheel");
+                break;
+            case 3:
+                console.log("Clicked right mouse button");
+                break;
+        }
+    });
+
 
     $("[data-trigger='dropdown']").on("mouseenter", function(){
         let submenu = $(this).parent().find(".submenu");
@@ -14,10 +31,12 @@ $(document).ready(function(){
 
 
     $("#append, #prepend, #replace").on("click", (event)=>{
-
+        event.preventDefault();
+        
         let element = $(event.currentTarget);
         let action = element.attr("id");
         let content = $(".text").val();
+        $()
 
         if(action == "append"){
             console.log("Appending",content);
